59af5e4bb07d6132a46824df2c4791ece3e832c0
Dima Lunich <dima.lunich@gmail.com>
2013-10-07 21:34:26 +0300

    [^] ctr fixes

--- hw2.rb
+++ hw2.rb
@@ -8,25 +8,21 @@ class AES
   end
 
   def decrypt_cbc(str, key)
-    prepare_for_decryption(str, key)
+    prepare_for_decryption(str, key, 16)
     do_decrypt_cbc
   end
 
   def decrypt_ctr(str, key)
-    prepare_for_decryption(str, key)
+    prepare_for_decryption(str, key, 16)
     do_decrypt_ctr
   end
 
 private
-  def block_size
-    16
-  end
-
-  def prepare_for_decryption(str, key)
+  def prepare_for_decryption(str, key, iv_size)
     @key = Utils.hex2str(key)
     arr = Utils.hex2str(str)
-    @iv = arr[0, block_size]
-    @data = arr[block_size..-1]
+    @iv = arr[0, iv_size]
+    @data = arr[iv_size..-1]
     # decryptor
     @cipher = OpenSSL::Cipher.new('AES128')
     @cipher.padding = 0
@@ -44,17 +40,19 @@ private
 
   def do_decrypt_ctr
     @result = ''
-    @data.each_slice(block_size) do |slice|
-      slice = align_block(slice)
-      dec = decrypt_block(@iv)
-      @result += dec ^ slice
+    i = 0
+    @data.each_slice(16) do |slice|
+      h, l = @iv.unpack("Q*")
+      l += i
+      dec = decrypt_block([h, l].pack("Q*"))
+      @result += slice ^ dec
     end
     @result
   end
 
   def do_decrypt_cbc
     @result = ''
-    @data.each_slice(block_size) do |slice|
+    @data.each_slice(16) do |slice|
       dec = decrypt_block(slice)
       @result += dec ^ @iv
       @iv = slice
@@ -64,8 +62,8 @@ private
 
   def do_encrypt
     @result = @iv
-    @data.each_slice(block_size) do |slice|
-      slice = align_block(slice)
+    @data.each_slice(16) do |slice|
+      slice = align_block(slice, 16)
       block_for_aes = @iv ^ slice
       @result += (@iv = encrypt_block(block_for_aes))
     end
@@ -82,8 +80,8 @@ private
     @cipher.update(block)
   end
 
-  def align_block(block)
-    if (delta = block_size - block.size) > 0
+  def align_block(block, size)
+    if (delta = size - block.size) > 0
       block += ([delta] * delta).pack("C*")
     end
     block
@@ -103,5 +101,9 @@ puts res = crypter.decrypt_cbc(enc, key)
 raise "INVALID" unless crypter.encrypt_cbc(res, key, enc[0, 32])
 
 enc = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329"
+key = "36f18357be4dbd77f050515c73fcf9f2"
+puts res = crypter.decrypt_ctr(enc, key)
+
+enc = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451"
 key = "36f18357be4dbd77f050515c73fcf9f2"
 puts res = crypter.decrypt_ctr(enc, key)
